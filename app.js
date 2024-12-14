126668513ba743861d5f7a12ff4b6ad8f592a8ce
yabibal <eshetieyabibal@gmail.com>
2024-12-14 16:23:12 -0500

    fix error

--- app.js
+++ app.js
@@ -15,7 +15,6 @@ app.use(cors());
 app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/books", bookRoutes);
-app.use(errorMiddleware);
 
 
 
--- controllers/authController.js
+++ controllers/authController.js
@@ -1,19 +1,23 @@
 import authService from "../services/authService.js";
 
-export const register = async (req, res, next) => {
+export const register = async (req, res) => {
   try {
     const user = await authService.register(req.body);
     res.status(201).json(user);
   } catch (error) {
-    next(error);
+    res.status(500).json({
+      message: error.message || "An error occurred during registration.",
+    });
   }
 };
 
-export const login = async (req, res, next) => {
+export const login = async (req, res) => {
   try {
     const userDetails = await authService.login(req.body);
     res.status(200).json({ userDetails });
   } catch (error) {
-    next(error);
+    res.status(401).json({
+      message: error.message || "Invalid credentials. Login failed.",
+    });
   }
 };
